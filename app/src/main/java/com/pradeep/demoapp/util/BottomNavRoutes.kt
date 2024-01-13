97f6716690728aef99a5866308761876f51914ba
Pradeep <pkdubey071@gmail.com>
2024-01-13 12:26:11 +0530

    Commit changes

--- app/src/main/java/com/pradeep/demoapp/util/BottomNavRoutes.kt
+++ app/src/main/java/com/pradeep/demoapp/util/BottomNavRoutes.kt
@@ -1,9 +1,7 @@
 package com.pradeep.demoapp.util
 
-/**
- * @author Ahmed Guedmioui
- */
 object BottomNavRoute {
     const val MEDIA_HOME_SCREEN = "media_home_screen"
     const val MEDIA_LIST_SCREEN = "media_list_screen"
+    //Singleton
 }
